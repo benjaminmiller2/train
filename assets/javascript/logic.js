f02cfe505a794ce60d98484f0063df3e307c65a3
Benjamin Miller <benjamin.miller2@northwestern.edu>
2018-03-29 14:37:50 -0500

    b

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,5 +1,7 @@
-  // Initialize Firebase
-  var config = {
+$(document).ready(function(){
+    
+//Initialize Firebase
+var config = {
     apiKey: "AIzaSyD05aibACnCkwyZq29dC3Wb3iFmwf-UpHo",
     authDomain: "train-project-646f7.firebaseapp.com",
     databaseURL: "https://train-project-646f7.firebaseio.com",
@@ -7,13 +9,14 @@
     storageBucket: "train-project-646f7.appspot.com",
     messagingSenderId: "676281918262"
   };
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
+let database = firebase.database();
 
- let name = "";
- let destination = "";
- let frequency = 0;
- let minutes = 0;
- let next = "";
+let name = "";
+let destination = "";
+let frequency = 0;
+let minutes = 0;
+let next = "";
 
 $(".js-submit").on("click", function(){
 
@@ -23,5 +26,33 @@ frequency = $(".js-tFrequency").val().trim();
 minutes = $(".js-tMinutesAway").val().trim();
 next = $(".js-tNextTrain").val().trim();
 
+
+
+database.ref().push({
+name: name,
+destination: destination,
+frequency: frequency,
+minutes: minutes,
+next: next,
+dateAdded: firebase.database.ServerValue.TIMESTAMP
+});
+
+
+});
+
+database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot){
+
+let newTrain = $("<tr>");
+let newName = $("<td class='t-data'>" + snapshot.val().name + "</td>");
+let newDestination = $("<td class='t-data'>" + snapshot.val().destination + "</td>");
+let newFrequency = $("<td class='t-data'>" + snapshot.val().frequency + "</td>");
+let newMinutes = $("<td class='t-data'>" + snapshot.val().minutes + "</td>");
+let newNext = $("<td class='t-data'>" + snapshot.val().next + "</td>");
+
+$(".t-table").append(newTrain);
+newTrain.append(newName).append(newDestination).append(newFrequency).append(newMinutes).append(newNext);
+
+console.log(snapshot.val().name);
+})
 
 })
