edbb989da2d1b938f10fcca71fb0012c1b430926
Benjamin Miller <benjamin.miller2@northwestern.edu>
2018-04-04 12:22:03 -0500

    updated readme and psuedo coding

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # train
+This app allows the user to create a train schedule based on the user's inputs.
+
+Simply fill in the fields provided - the train's name, destination, frequency, and initial departure - and the app will calculate the next train due, and the time until its arrival.
--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -10,14 +10,14 @@ var config = {
     messagingSenderId: "676281918262"
   };
 firebase.initializeApp(config);
-let database = firebase.database();
 
+//global variables
+let database = firebase.database();
+//input variables and variables used for moment's calculations
 let name = "";
 let destination = "";
 let firstTrain = "";
 let frequency = "";
-
-
 let backDate = "";
 let currentTime = "";
 let diffTime = "";
@@ -31,50 +31,57 @@ let nextTrainFormatted = "";
 $(".js-submit").on("click", function(){
     event.preventDefault();
 
-name = $(".js-tName").val().trim();
-destination = $(".js-tDestination").val().trim();
-firstTrain = $(".js-tFirst").val().trim();
-frequency = $(".js-tFrequency").val().trim();
-backDate = moment(firstTrain, "hh:mm").subtract(1, "months");
-currentTime = moment();
-diffTime = moment().diff(moment(backDate), "minutes");
-remaining = diffTime % frequency;
-minutesAway = frequency - remaining;
-nextTrain = moment().add(minutesAway, "minutes");
-nextTrainFormatted = moment(nextTrain).format("hh:mm");
+    //assigning user inputs to variables
+    name = $(".js-tName").val().trim();
+    destination = $(".js-tDestination").val().trim();
+    firstTrain = $(".js-tFirst").val().trim();
+    frequency = $(".js-tFrequency").val().trim();
 
+    //assigning moment functions to variables
+    backDate = moment(firstTrain, "hh:mm").subtract(1, "months");
+    currentTime = moment();
+    diffTime = moment().diff(moment(backDate), "minutes");
+    remaining = diffTime % frequency;
+    minutesAway = frequency - remaining;
+    nextTrain = moment().add(minutesAway, "minutes");
+    nextTrainFormatted = moment(nextTrain).format("hh:mm");
 
-database.ref().push({
-name: name,
-destination: destination,
-frequency: frequency,
-firstTrain: firstTrain,
-minutesAway: minutesAway,
-nextTrainFormatted: nextTrainFormatted,
-dateAdded: firebase.database.ServerValue.TIMESTAMP
+        //pushes variable values to their corresponding fields in firebase
+        database.ref().push({
+        name: name,
+        destination: destination,
+        frequency: frequency,
+        firstTrain: firstTrain,
+        minutesAway: minutesAway,
+        nextTrainFormatted: nextTrainFormatted,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+            //clears user inputs from the input fields
+            name = $(".js-tName").val("");
+            destination = $(".js-tDestination").val("");
+            firstTrain = $(".js-tFirst").val("");
+            frequency = $(".js-tFrequency").val("");
+
 });
 
-name = $(".js-tName").val("");
-destination = $(".js-tDestination").val("");
-firstTrain = $(".js-tFirst").val("");
-frequency = $(".js-tFrequency").val("");
-
-});
-
+//Upon recieving new data from the app, the data base returns the new values to the app
+//and displays them in the main table
 database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot){
 
-let newTrain = $("<tr>");
-let newName = $("<td class='t-data'>" + snapshot.val().name + "</td>");
-let newDestination = $("<td class='t-data'>" + snapshot.val().destination + "</td>");
-let newFrequency = $("<td class='t-data'>" + snapshot.val().frequency + "</td>");
-let newNextTrain = $("<td class='t-data'>" + snapshot.val().nextTrainFormatted + "</td>");
-let newMinutesAway = $("<td class='t-data'>" + snapshot.val().minutesAway + "</td>");
+    //creates variables and assigns instructions to create new html elements
+    //specifically, these create new table rows and data corresponding to newly recieved
+    //values in the database
+    let newTrain = $("<tr>");
+    let newName = $("<td class='t-data'>" + snapshot.val().name + "</td>");
+    let newDestination = $("<td class='t-data'>" + snapshot.val().destination + "</td>");
+    let newFrequency = $("<td class='t-data'>" + snapshot.val().frequency + "</td>");
+    let newNextTrain = $("<td class='t-data'>" + snapshot.val().nextTrainFormatted + "</td>");
+    let newMinutesAway = $("<td class='t-data'>" + snapshot.val().minutesAway + "</td>");
 
+        //appends the new html elements to the apps main table
+        $(".t-table").append(newTrain);
+        newTrain.append(newName).append(newDestination).append(newFrequency).append(newNextTrain).append(newMinutesAway);
 
-$(".t-table").append(newTrain);
-newTrain.append(newName).append(newDestination).append(newFrequency).append(newNextTrain).append(newMinutesAway);
-
-console.log(snapshot.val().name);
 
 });
 
